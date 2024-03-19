51d7ed99857a378a29eedfeb9b6a9db815db2a42
Satyadev <satyadevba18@gmail.com>
2024-03-19 18:35:19 +0530

    added all the changes

--- LinkList/LinkListUse.java
+++ LinkList/LinkListUse.java
@@ -1,11 +1,31 @@
 package LinkList;
 
 public class LinkListUse {
+    public static void print(Node<Integer> head){
+        // Node<Integer> temp = head;
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+        // head=temp;
+        // while (head != null) {
+        //     System.out.print(head.data + " ");
+        //     head = head.next;
+        // }
+    }
     public static void main(String[] args) {
         Node<Integer> node1 = new Node<Integer>(14);
-        System.out.println(node1.data);
-        System.out.println(node1.next);
+        // System.out.println(node1.data);
+        // System.out.println(node1.next);
         Node<Integer> node2 = new Node<Integer>(29);
         node1.next = node2; 
+        Node<Integer> node3 = new Node<Integer>(28);
+        node2.next = node3;
+       print(node1);
+       print(node1);
+
+      
+
     }
 }
